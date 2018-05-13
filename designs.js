d5157ee6b0c41613273fb55ab6f1b7540d36fdcb
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-05-14 01:47:54 +0300

    Added two cats and their names

--- designs.js
+++ designs.js
@@ -1,8 +1,35 @@
 "use strict";
-let kitten = document.getElementById('kitten');
-let tapCount = document.getElementById('tapCount');
-let tapNum = 0;
-kitten.addEventListener('click', function() {
-  tapNum += 1;
-  tapCount.textContent = tapNum;
+
+var Cat = function() {
+  this.score = 0;
+}
+
+Cat.prototype.updateScore = function() {
+    this.score += 1;
+    this.board.textContent = this.score;
+};
+
+Cat.prototype.printName = function() {
+  let nameHeader = "<h1 class=\"name\">" + this.name + "</h1>";
+  this.container.insertAdjacentHTML('afterbegin', nameHeader);
+}
+
+let kitten1 = new Cat();
+kitten1.name = "Ashy";
+kitten1.board = document.getElementById('tapCount1');
+kitten1.container = document.getElementById('kitten1-container');
+kitten1.printName();
+
+let kitten2 = new Cat();
+kitten2.name = "Luna";
+kitten2.board = document.getElementById('tapCount2');
+kitten2.container = document.getElementById('kitten2-container');
+kitten2.printName();
+
+kitten1.container.addEventListener("click", function() {
+  kitten1.updateScore();
+});
+
+kitten2.container.addEventListener("click", function() {
+  kitten2.updateScore();
 });
