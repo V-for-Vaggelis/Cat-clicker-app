4803e9fd5803f7d4e5e6338872409f5e72caa77f
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-05-15 00:10:11 +0300

    Created basic 5 cat format and objects

--- designs.js
+++ designs.js
@@ -1,37 +1,65 @@
 "use strict";
 document.addEventListener('DOMContentLoaded', e => {
   // This event listener is a vanilla js way to ensure js executes after DOM load
+  let catContainer = document.getElementById("cat-info");
+  let nameList = document.getElementById("catList");
   var Cat = function() {
     this.score = 0;
   }
 
   Cat.prototype.updateScore = function() {
     this.score += 1;
-    this.board.textContent = this.score;
+    let scoreHtml = "<h1 class=\"count\">" + this.score + "</h1>";
+    catContainer.insertAdjacentHTML("beforeend", scoreHtml);
   };
 
   Cat.prototype.printName = function() {
-    let nameHeader = "<h1 class=\"name\">" + this.name + "</h1>";
-    this.container.insertAdjacentHTML('afterbegin', nameHeader);
+    let nameHtml = "<h1 class=\"name\">" + this.name + "</h1>";
+    catContainer.insertAdjacentHTML("beforeend", nameHtml);
+  };
+
+  Cat.prototype.printPicture = function() {
+    let picHtml = "<img alt=" + this.alt + "class=\"cat\" src=" + this.link + "></img>";
+    catContainer.insertAdjacentHTML("beforeend", picHtml);
+  };
+
+  Cat.prototype.changeCat = function() {
+    // The order here is really important!
+    catContainer.innerHTML = "";
+    this.printName();
+    this.printPicture();
+    this.updateScore();
   }
 
+// Let's instantiate all the cat objects and store them in an array
   let ashie = new Cat();
   ashie.name = "Ashie";
-  ashie.board = document.getElementById('tapCountAshie');
-  ashie.container = document.getElementById('ashie-container');
-  ashie.printName();
+  ashie.alt = "A grey kitty";
+  ashie.link = "ashie.png"
 
   let luna = new Cat();
   luna.name = "Luna";
-  luna.board = document.getElementById('tapCountLuna');
-  luna.container = document.getElementById('luna-container');
-  luna.printName();
+  luna.alt = "A white-orange cat with a mark on it's nose";
+  luna.link = "luna.jpeg";
 
-  ashie.container.addEventListener("click", function() {
-    ashie.updateScore();
-  });
+  let cleo = new Cat();
+  cleo.name = "Cleo";
+  cleo.alt = "A beautiful white-grey cat with blue eyes";
+  cleo.link = "cleo.png";
 
-  luna.container.addEventListener("click", function() {
-    luna.updateScore();
-  });
+  let grouchy = new Cat();
+  grouchy.name = "Grouchy";
+  grouchy.alt = "A grouchy little blonde kitten";
+  grouchy.link = "grouchy.jpg";
+
+  let grumpy = new Cat();
+  grumpy.name = "Grumpy";
+  grumpy.alt = "A grumpy little kitten spitting it's food";
+  grumpy.link = "grumpy.jpg";
+
+  let allCats = [ashie, luna, cleo, grouchy, grumpy];
+  for (let cat of allCats) {
+    let nameCode = "<li id=" + cat.name +">" + cat.name + "</li>";
+    nameList.insertAdjacentHTML("beforeend", nameCode);
+  }
 });
