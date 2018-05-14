57d0b6ad306d2421853ffc146b5a86327edd0ea7
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-05-14 16:10:39 +0300

    --amend

--- designs.js
+++ designs.js
@@ -1,35 +1,37 @@
 "use strict";
+document.addEventListener('DOMContentLoaded', e => {
+  // This event listener is a vanilla js way to ensure js executes after DOM load
+  var Cat = function() {
+    this.score = 0;
+  }
 
-var Cat = function() {
-  this.score = 0;
-}
-
-Cat.prototype.updateScore = function() {
+  Cat.prototype.updateScore = function() {
     this.score += 1;
     this.board.textContent = this.score;
-};
+  };
 
-Cat.prototype.printName = function() {
-  let nameHeader = "<h1 class=\"name\">" + this.name + "</h1>";
-  this.container.insertAdjacentHTML('afterbegin', nameHeader);
-}
+  Cat.prototype.printName = function() {
+    let nameHeader = "<h1 class=\"name\">" + this.name + "</h1>";
+    this.container.insertAdjacentHTML('afterbegin', nameHeader);
+  }
 
-let ashie = new Cat();
-ashie.name = "Ashie";
-ashie.board = document.getElementById('tapCountAshie');
-ashie.container = document.getElementById('ashie-container');
-ashie.printName();
+  let ashie = new Cat();
+  ashie.name = "Ashie";
+  ashie.board = document.getElementById('tapCountAshie');
+  ashie.container = document.getElementById('ashie-container');
+  ashie.printName();
 
-let luna = new Cat();
-luna.name = "Luna";
-luna.board = document.getElementById('tapCountLuna');
-luna.container = document.getElementById('luna-container');
-luna.printName();
+  let luna = new Cat();
+  luna.name = "Luna";
+  luna.board = document.getElementById('tapCountLuna');
+  luna.container = document.getElementById('luna-container');
+  luna.printName();
 
-ashie.container.addEventListener("click", function() {
-  ashie.updateScore();
-});
+  ashie.container.addEventListener("click", function() {
+    ashie.updateScore();
+  });
 
-luna.container.addEventListener("click", function() {
-  luna.updateScore();
+  luna.container.addEventListener("click", function() {
+    luna.updateScore();
+  });
 });
